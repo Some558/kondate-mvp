4ba95ccda8b0c395dad5ecebefb73286d6b8c963
Some558 <167545573+Some558@users.noreply.github.com>
2024-08-26 15:17:58 +0900

    一覧表示を献立一覧に変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')">
-                        投稿一覧
+                        献立1week
                     </x-nav-link>
                     
                     <x-nav-link :href="route('post.create')" :active="request()->routeIs('post.create')">
