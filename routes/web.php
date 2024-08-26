aac43240c04b648973cdd31a1ec965679cf72c4b
Some558 <167545573+Some558@users.noreply.github.com>
2024-08-26 09:17:14 +0900

    dashboard表示変更

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::resource('post', PostController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
