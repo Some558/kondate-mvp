3bb0bc370b0016c66d4eb5a12e6ba1f51863c7d1
Some558 <167545573+Some558@users.noreply.github.com>
2024-09-01 22:41:48 +0900

    create画面編集

--- resources/views/menus/create.blade.php
+++ resources/views/menus/create.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create Monday\'s Menu') }}
+            献立作成
         </h2>
     </x-slot>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/6.9.96/css/materialdesignicons.min.css" rel="stylesheet">
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -12,16 +14,17 @@
                     <form action="{{ route('menus.store') }}" method="POST">
                         @csrf
                         <div class="mb-4">
-                            <label for="main_dish" class="block text-gray-700 text-sm font-bold mb-2">メインメニュー:</label>
-                            <input type="text" name="main_dish" id="main_dish" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                            
+                            <label for="main_dish" class="block text-indigo-700 text-xl font-bold mb-4">メインメニュー:</label>
+                            <input type="text" name="main_dish" id="main_dish" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4" required>
                         </div>
                         <div class="mb-4">
-                            <label for="sub_dish1" class="block text-gray-700 text-sm font-bold mb-2">サイドメニュー 1:</label>
-                            <input type="text" name="sub_dish1" id="sub_dish1" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                            <label for="sub_dish1" class="block text-indigo-700 text-xl font-bold mb-4">サイドメニュー 1:</label>
+                            <input type="text" name="sub_dish1" id="sub_dish1" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4" required>
                         </div>
                         <div class="mb-6">
-                            <label for="sub_dish2" class="block text-gray-700 text-sm font-bold mb-2">サイドメニュー 2:</label>
-                            <input type="text" name="sub_dish2" id="sub_dish2" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required>
+                            <label for="sub_dish2" class="block text-indigo-700 text-xl font-bold mb-4">サイドメニュー 2:</label>
+                            <input type="text" name="sub_dish2" id="sub_dish2" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4" required>
                         </div>
                         <div class="flex items-center justify-between">
                             <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
